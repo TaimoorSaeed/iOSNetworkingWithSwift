add2fcdc176c1c005ec903b254ad559681a658bd
TaimoorSaeed <taimoor_saeed@hotmail.com>
2017-04-03 19:21:43 +0500

    a

--- SleepingInTheLibrary/SleepingInTheLibrary/ViewController.swift
+++ SleepingInTheLibrary/SleepingInTheLibrary/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 // MARK: - ViewController: UIViewController
 
 class ViewController: UIViewController {
-
+    
     // MARK: Outlets
     
     @IBOutlet weak var photoImageView: UIImageView!
@@ -40,8 +40,91 @@ class ViewController: UIViewController {
     
     // MARK: Make Network Request
     
+//    private func getImageFromFlickr() {
+//        
+//        let url = URL(string: "\(Constants.Flickr.APIBaseURL)?\(Constants.FlickrParameterKeys.Method)=\(Constants.FlickrParameterValues.GalleryPhotosMethod)&\(Constants.FlickrParameterKeys.APIKey)=\(Constants.FlickrParameterValues.APIKey)&\(Constants.FlickrParameterKeys.GalleryID)=\(Constants.FlickrParameterValues.GalleryID)&\(Constants.FlickrParameterKeys.Extras)=\(Constants.FlickrParameterValues.MediumURL)&\(Constants.FlickrParameterKeys.Format)=\(Constants.FlickrParameterValues.ResponseFormat)&\(Constants.FlickrParameterKeys.NoJSONCallback)=\(Constants.FlickrParameterValues.DisableJSONCallback)")!
+//        
+//        print(url)
+//        
+//    }
+    
+    private func escapedParameters(_ parameters: [String:AnyObject]) -> String {
+        
+        if parameters.isEmpty {
+            return ""
+        } else {
+            var keyValuePairs = [String]()
+            
+            for (key, value) in parameters {
+                
+                // make sure that it is a string value
+                let stringValue = "\(value)"
+                
+                // escape it
+                let escapedValue = stringValue.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+                
+                // append it
+                keyValuePairs.append(key + "=" + "\(escapedValue!)")
+                
+            }
+            
+            return "?\(keyValuePairs.joined(separator: "&"))"
+        }
+    }
+    
     private func getImageFromFlickr() {
         
-        // TODO: Write the network code here!
-    }
-}
+        let methodParameters = [
+            Constants.FlickrParameterKeys.Method: Constants.FlickrParameterValues.GalleryPhotosMethod,
+            Constants.FlickrParameterKeys.APIKey: Constants.FlickrParameterValues.APIKey,
+            Constants.FlickrParameterKeys.GalleryID: Constants.FlickrParameterValues.GalleryID,
+            Constants.FlickrParameterKeys.Extras: Constants.FlickrParameterValues.MediumURL,
+            Constants.FlickrParameterKeys.Format: Constants.FlickrParameterValues.ResponseFormat,
+            Constants.FlickrParameterKeys.NoJSONCallback: Constants.FlickrParameterValues.DisableJSONCallback
+        ]
+        
+        let urlString = Constants.Flickr.APIBaseURL + escapedParameters(methodParameters as [String:AnyObject])
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url)
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            private func getImageFromFlickr() {
+                
+                // [creating the url and request]...
+                
+                // create network request
+                let task = session.dataTask(with: request) { (data, response, error) in
+                    
+                    // if an error occurs, print it and re-enable the UI
+                    func displayError(_ error: String) {
+                        print(error)
+                        print("URL at time of error: \(url)")
+                        performUIUpdatesOnMain {
+                            self.setUIEnabled(true)
+                        }
+                    }
+                    
+                    // no error, woohoo!
+                    if error == nil {
+                        
+                        // there was data returned
+                        if let data = data {
+                            
+                            let parsedResult: [String:AnyObject]!
+                            do {
+                                parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:AnyObject]
+                            } catch {
+                                displayError("Could not parse the data as JSON: '\(data)'")
+                                return
+                            }
+                            
+                            print(parsedResult)
+                        }
+                    }
+                }
+                
+                // start the task!
+                task.resume()
+                
+};
